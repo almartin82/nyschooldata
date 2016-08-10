d67d916f5d142ff98490a73ebf9839aa114dc3ba
almartin82 <almartin@gmail.com>
2016-08-10 11:38:44 -0400

    s #34 and #31

--- tests/testthat/helper_constants.R
+++ tests/testthat/helper_constants.R
@@ -1,11 +1,5 @@
 # #this gets run before tests
 
-#2015 bits and pieces
-ex_2015 <- get_raw_assess_db(2015, verbose = FALSE)
-clean_2015 <- clean_assess_db(ex_2015, 2015, verbose = FALSE)
-agg_2015 <- aggregate_grades(clean_2015)
-
-final_2015 <- dplyr::bind_rows(clean_2015, agg_2015)
-
-#2016 using the wrapper
-final_2016 <- fetch_and_aggregate_assess_db(2016, verbose = TRUE)
+assess_all <- dplyr::bind_rows(assess_13, assess_14, assess_15, assess_16)
+growth_all <- dplyr::bind_rows(assess_growth_13_14, assess_growth_14_15, assess_growth_15_16)
+cohort_growth_all <- dplyr::bind_rows(cohort_growth_13_14, cohort_growth_14_15, cohort_growth_15_16)
