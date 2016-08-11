213f9ba5f9d599f930981f9e4dc58e6c0bc3caa8
Andrew Martin <amartin@publicprep.org>
2016-08-11 17:04:45 -0400

    more prof_hist stuff

--- tests/testthat/test_proficiency_hist.R
+++ tests/testthat/test_proficiency_hist.R
@@ -5,11 +5,23 @@ test_that("basic_proficiency_hist produces a plot", {
 
   ex <- p_proficiency_hist(
     assess_db_all = assess_all,
-    bedscodes = c('310100860866', '320800860940'),
+    bedscodes = c('310100860866', ''),
     subjects = 'ELA',
     grades = c(5, 6)
   )
 
   expect_is(ex, 'ggplot')
+})
 
+
+test_that("basic_proficiency_hist produces a plot", {
+
+  ex <- p_proficiency_hist(
+    assess_db_all = assess_all,
+    bedscodes = c('320800860940', '000000000001'),
+    subjects = 'ELA',
+    grades = c(5, 6)
+  )
+
+  expect_is(ex, 'ggplot')
 })
