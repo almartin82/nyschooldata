cff0289b95886379751f4e55a348323268d11690
almartin82 <almartin@gmail.com>
2016-08-15 17:35:03 -0400

    grid packages

--- tests/testthat/test_proficiency_hist.R
+++ tests/testthat/test_proficiency_hist.R
@@ -17,7 +17,7 @@ test_that("basic_proficiency_hist produces a plot", {
 test_that("basic_proficiency_hist produces a plot", {
 
   ex <- p_proficiency_hist(
-    assess_db_all = assess_all,
+    assess_db_all = clean_all,
     bedscodes = c('320800860940', '000000000001'),
     subjects = 'ELA',
     grades = c(5, 6)
@@ -30,7 +30,7 @@ test_that("basic_proficiency_hist produces a plot", {
 test_that("p_proficiency_hist_single_subj_sch_comparison", {
 
   sch_vs_nyc_math <- p_proficiency_hist_single_subj_sch_comparison(
-    assess_db_all = ppn_assess_all,
+    assess_db_all = agg_all,
     bedscodes = c('320800860940', '000000000001'),
     subjects =  'Math',
     grades = 'All'
