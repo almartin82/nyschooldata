2669691a58064923abc312eee3c1bedd5f8fc00f
almartin82 <almartin@gmail.com>
2017-08-02 15:04:14 -0400

    more tests on assess_db

--- tests/testthat/test_assess_db.R
+++ tests/testthat/test_assess_db.R
@@ -2,6 +2,7 @@ context("assess_db")
 
 ex <- get_raw_assess_db(2015)
 clean_ex <- fetch_assess_db(2013)
+assess_ex <- assess_db(2013)
 
 test_that("get_raw_assess_db correctly reads a NY state file", {
 
@@ -14,4 +15,14 @@ test_that("fetch_aggregate_percentile_assess_db correctly reads
 
   expect_is(clean_ex, 'data.frame')
   expect_equal(nrow(clean_ex), 562016)
+})
+
+
+test_that("assess_db returns expected data", {
+  expect_equal(
+    names(assess_ex), c('assess', 'aggregates')
+  )
+
+  expect_equal(dim(assess_ex$assess)[1], 435244)
+  expect_equal(dim(assess_ex$aggregates[1], 126772))
 })
